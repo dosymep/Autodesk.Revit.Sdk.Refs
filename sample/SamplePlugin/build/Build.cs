9be74c1763a4092570ef84d9f6d0d77ea4908f52
dosymep <dosymep@gmail.com>
2023-11-08 19:03:19 +0300

    update sample project

--- sample/SamplePlugin/build/Build.cs
+++ sample/SamplePlugin/build/Build.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using dosymep.Nuke.RevitVersions;
+
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
@@ -9,7 +11,7 @@ using Nuke.Components;
 
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-class Build : NukeBuild {
+class Build : NukeBuild, IHazSolution {
     /// Support plugins are available for:
     /// - JetBrains ReSharper        https://nuke.build/resharper
     /// - JetBrains Rider            https://nuke.build/rider
@@ -21,45 +23,69 @@ class Build : NukeBuild {
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     /// <summary>
-    /// The name of the project to be compiled.
-    /// </summary>
-    string ProjectName => "SamplePlugin";
-
-    /// <summary>
     /// Output directory.
     /// </summary>
-    AbsolutePath OutputDirectory => RootDirectory / "bin";
-
+    AbsolutePath Output => RootDirectory / "bin";
+    
     /// <summary>
-    /// Project directory.
+    /// Min Revit version.
     /// </summary>
-    AbsolutePath ProjectDirectory => RootDirectory / ProjectName;
+    [Parameter("Min Revit version.")]
+    readonly RevitVersion MinVersion = RevitVersion.Rv2020;
 
     /// <summary>
-    /// Cleans <see cref="Build.OutputDirectory"/> and build and obj folders in project.
+    /// Max Revit version.
+    /// </summary>
+    [Parameter("Max Revit version.")]
+    readonly RevitVersion MaxVersion = RevitVersion.Rv2024;
+    
+    [Parameter("Build Revit versions.")] readonly RevitVersion[] RevitVersions = new RevitVersion[0];
+
+    IEnumerable<RevitVersion> BuildRevitVersions;
+
+    protected override void OnBuildInitialized() {
+        base.OnBuildInitialized();
+        BuildRevitVersions = RevitVersions.Length > 0
+            ? RevitVersions
+            : RevitVersion.GetRevitVersions(MinVersion, MaxVersion);
+    }
+
+    /// <summary>
+    /// Cleans <see cref="Output"/> and build and obj folders in project.
     /// </summary>
     Target Clean => _ => _
         .Executes(() => {
-            OutputDirectory.CreateOrCleanDirectory();
-            ProjectDirectory.GlobDirectories("**/bin", "**/obj")
+            Output.CreateOrCleanDirectory();
+            RootDirectory.GlobDirectories("**/bin", "**/obj")
                 .Where(item => item != RootDirectory / "build" / "bin")
                 .Where(item => item != RootDirectory / "build" / "obj")
                 .DeleteDirectories();
+        });
+    
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() => {
+            DotNetRestore(s => s
+                .SetProjectFile(((IHazSolution) this).Solution));
         });
 
     /// <summary>
     /// Compile project with all revit versions.
     /// </summary>
     Target Compile => _ => _
-        .DependsOn(Clean)
+        .DependsOn(Restore)
         .Executes(() => {
             DotNetBuild(s => s
                 .EnableForce()
                 .DisableNoRestore()
-                .SetProjectFile(ProjectName)
                 .SetConfiguration(Configuration)
+                .SetProjectFile(((IHazSolution) this).Solution)
                 .When(IsServerBuild, _ => _
                     .EnableContinuousIntegrationBuild())
-                .SetProperty("RevitVersion", 2016));
+                .CombineWith(BuildRevitVersions, (settings, version) => {
+                    return settings
+                        .SetOutputDirectory(Output / version)
+                        .SetProperty("RevitVersion", (int) version);
+                }));
         });
 }
